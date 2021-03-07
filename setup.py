0486fcd51d0f79244407a042a75c9b081be59dac
Derek Anderson <derek@armyofevilrobots.com>
2021-03-07 10:31:31 -0800

    Add package discovery to fix build bugs.

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="Bot-a-Plot", # Replace with your own username
-    version="0.0.1",
+    name="Bot-a-Plot",
+    version="0.1.0",
     author="Derek Anderson",
     author_email="enki+botaplot@armyofevilrobots.com",
     description="Plotter management and generative art environment",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    zip_safe=False,
     url="https://armyofevilrobots.com",
     package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     scripts=["scripts/botaplot", "scripts/botaplot_svg2gcode", "scripts/botaplot-print"],
     classifiers=[
         "Programming Language :: Python :: 3",
